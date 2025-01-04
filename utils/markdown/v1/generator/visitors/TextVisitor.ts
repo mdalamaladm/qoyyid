439d1630bc05d714c98a85a158e57f31a006df2c
mdalamaladm <muhammaddzakyalam@gmail.com>
2025-01-04 22:55:26 +0300

    fix(markdown): remove unnecessary conditional in TextVisitor

--- utils/markdown/v1/generator/visitors/TextVisitor.ts
+++ utils/markdown/v1/generator/visitors/TextVisitor.ts
@@ -1,7 +1,5 @@
 export default class TextVisitor {
   static visit (node, render) {
-    if (node.isWord) return render?.text?.(node.value, node.isWord) || `${node.value}`
-    
     return render?.text?.(node.value, node.isWord) || node.value
   }
 }
