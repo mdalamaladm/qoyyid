678cc0ccdcb0f7165e58df4a4a9807d669bcd61f
mdalamaladm <muhammaddzakyalam@gmail.com>
2025-01-04 22:46:04 +0300

    feat(icon): add new: preview and difference

--- components/Icon/index.tsx
+++ components/Icon/index.tsx
@@ -2,10 +2,12 @@ import add from '@/components/Icon/add'
 import chevronLeft from '@/components/Icon/chevronLeft'
 import chevronRight from '@/components/Icon/chevronRight'
 import close from '@/components/Icon/close'
+import difference from '@/components/Icon/difference'
 import edit from '@/components/Icon/edit'
 import logout from '@/components/Icon/logout'
 import menu from '@/components/Icon/menu'
 import more from '@/components/Icon/more'
+import preview from '@/components/Icon/preview'
 import save from '@/components/Icon/save'
 
 let icons = {
@@ -13,10 +15,12 @@ let icons = {
   chevronLeft,
   chevronRight,
   close,
+  difference,
   edit,
   logout,
   menu,
   more,
+  preview,
   save,
 }
 
